95d156b84874d6bc47aa2c8bad2180c8393105cc
Maija Mednieks <mmednieks@gmail.com>
2016-08-19 02:28:54 -0400

    better newline removal

--- js/morse.js
+++ js/morse.js
@@ -5,7 +5,7 @@
 
 function morseToLatin(input) {
 	var output = "";
-	input.replace(/^\s+|\s+$/g, '');
+	input..replace(/(\r\n|\n|\r)/gm,"");
 	var letters = input.split(token);
 	for (var i in letters) {
 		if (letterMapping[letters[i]] == undefined) {
